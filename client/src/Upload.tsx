35f5522a7863b220f5bc4bb8abb3c3a36230f799
coopertweak <scottrcoop@gmail.com>
2023-09-15 16:01:07 -0500

    Uploads the html file after input

--- client/src/Upload.tsx
+++ client/src/Upload.tsx
@@ -4,39 +4,52 @@ import { upload } from "@spheron/browser-upload";
 import "./Upload.css";
 
 function Upload() {
-  const [file, setFile] = useState<File | null>(null);
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [uploadLink, setUploadLink] = useState("");
   const [dynamicLink, setDynamicLink] = useState("");
+  const [profilePic, setProfilePic] = useState<File | null>(null);
+  const [bio, setBio] = useState("");
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleProfilePicChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = event.target.files ? event.target.files[0] : null;
-    setFile(selectedFile);
-    setUploadLink("");
-    setDynamicLink("");
+    setProfilePic(selectedFile);
   };
-
-  const handleSelectFile = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
+  
+  const handleBioChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setBio(event.target.value);
   };
 
   const handleUpload = async () => {
-    if (!file) {
-      alert("No file selected");
+    if (!profilePic || !bio) {
+      alert("Please enter a profile picture and biography");
       return;
     }
-
+  
+    // Create the HTML content
+    const htmlContent = `
+      <html>
+        <head>
+          <title>User Profile</title>
+        </head>
+        <body>
+          <img src="${URL.createObjectURL(profilePic)}" alt="Profile Picture">
+          <p>${bio}</p>
+        </body>
+      </html>
+    `;
+  
+    // Create a File object representing the HTML file
+    const htmlFile = new File([htmlContent], "profile.html", {type : 'text/html'});
+  
+    // Upload the HTML file to Spheron
     try {
       setIsLoading(true);
       const response = await fetch("http://localhost:8111/initiate-upload");
       const responseJson = await response.json();
-      const uploadResult = await upload([file], {
+      const uploadResult = await upload([htmlFile], {
         token: responseJson.uploadToken,
       });
-
+  
       setUploadLink(uploadResult.protocolLink);
       setDynamicLink(uploadResult.dynamicLinks[0]);
     } catch (err) {
@@ -50,7 +63,7 @@ function Upload() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
+  
         {isLoading ? (
           <>Uploading...</>
         ) : (
@@ -58,31 +71,32 @@ function Upload() {
             <p>Upload Content to IPFS</p>
             <div className="flex gap-32">
               <div className="">
-                <div
-                  className="button-con button-53"
-                  onClick={handleSelectFile}
-                >
-                  Select File
+                <label className="button-con button-53" htmlFor="profilePic">
+                  Select Profile Picture
                   <input
-                    id="file"
+                    id="profilePic"
                     type="file"
-                    ref={fileInputRef}
-                    onChange={handleFileChange}
+                    accept="image/*"
+                    onChange={handleProfilePicChange}
                     className="w-full h-full"
                     style={{ display: "none" }}
                   />
-                </div>
+                </label>
                 <div className="flex-1 flex items-center pl-4 text-sm -rotate-2">
-                  {file ? file?.name : "No file selected"}
+                  {profilePic ? profilePic?.name : "No file selected"}
+                </div>
+                <div>
+                  <label htmlFor="bio">Biography:</label>
+                  <textarea id="bio" onChange={handleBioChange}></textarea>
                 </div>
               </div>
               <div className="flex flex-col">
-                <div
+                <button
                   className="button-con button-53 h-12"
                   onClick={handleUpload}
                 >
                   Upload
-                </div>
+                </button>
                 {uploadLink && (
                   <a
                     className="text-sm mt-4 -rotate-2"
@@ -107,7 +121,7 @@ function Upload() {
         )}
       </header>
     </div>
-  );
+  );  
 }
 
 export default Upload;
